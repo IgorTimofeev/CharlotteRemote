4e9083d184b2f2a6343ace583bb1af173a7cfee9
IgorTimofeev <methodman1@mail.ru>
2024-08-23 20:11:45 +0300

    cyka

--- src/transceiver/transceiver.h
+++ src/transceiver/transceiver.h
@@ -2,7 +2,25 @@
 #pragma once
 
 #include "Arduino.h"
-#include <RadioLib.h>
+#include <Ra01S.h>
+
+#define RF_FREQUENCY                                915000000 // Hz  center frequency
+#define TX_OUTPUT_POWER                             22        // dBm tx output power
+#define LORA_BANDWIDTH                              4         // bandwidth
+// 2: 31.25Khz
+// 3: 62.5Khz
+// 4: 125Khz
+// 5: 250KHZ
+// 6: 500Khz
+#define LORA_SPREADING_FACTOR                       7         // spreading factor [SF5..SF12]
+#define LORA_CODINGRATE                             1         // [1: 4/5,
+//  2: 4/6,
+//  3: 4/7,
+//  4: 4/8]
+
+#define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
+#define LORA_PAYLOADLENGTH                          0         // 0: Variable length packet (explicit header)
+// 1..255  Fixed length packet (implicit header)
 
 
 struct GovnoPacket {
@@ -26,18 +44,9 @@ class Transceiver {
 		void begin() {
 			Serial.println("[SX1262] Initializing");
 
-			auto state = _radio.begin(
-				434.0,
-				125.0,
-				9,
-				7,
-				RADIOLIB_SX126X_SYNC_WORD_PRIVATE,
-				10,
-				8,
-				0
-			);
+			auto state = _radio.begin(RF_FREQUENCY, TX_OUTPUT_POWER);
 
-			if (state == RADIOLIB_ERR_NONE) {
+			if (state == ERR_NONE) {
 
 			}
 			else {
@@ -47,6 +56,15 @@ class Transceiver {
 				while (true)
 					delay(100);
 			}
+
+			_radio.LoRaConfig(
+				LORA_SPREADING_FACTOR,
+				LORA_BANDWIDTH,
+				LORA_CODINGRATE,
+				LORA_PREAMBLE_LENGTH,
+				LORA_PAYLOADLENGTH,
+				true,               //crcOn
+				false);             //invertIrq
 		}
 
 		void tick() {
@@ -59,39 +77,37 @@ class Transceiver {
 		}
 
 	private:
-		SX1262 _radio = new Module(
-			4,
-			RADIOLIB_NC,
-			33,
-			32
+		SX126x _radio = SX126x(
+			4,               //Port-Pin Output: SPI select
+			33,               //Port-Pin Output: Reset
+			32                //Port-Pin Input:  Busy
 		);
 
 		uint32_t _deadline = 0;
 
 		void _receive() {
 			uint8_t buffer[255];
-			auto state = _radio.receive(buffer, 255);
 
-			if (state == RADIOLIB_ERR_NONE) {
+			auto len = _radio.Receive(buffer, 255);
+
+			if (len > 0) {
 				// packet was successfully received
 				Serial.println("KAKOITA PAKET");
+
+				int8_t rssi, snr;
+				_radio.GetPacketStatus(&rssi, &snr);
 
 				// print the RSSI (Received Signal Strength Indicator)
 				// of the last received packet
 				Serial.print("[SX1262] RSSI: ");
-				Serial.print(_radio.getRSSI());
+				Serial.print(rssi);
 				Serial.println(" dBm");
 
 				// print the SNR (Signal-to-Noise Ratio)
 				// of the last received packet
 				Serial.print("[SX1262] SNR: ");
-				Serial.print(_radio.getSNR());
+				Serial.print(snr);
 				Serial.println(" dB");
-
-				// print frequency error
-				Serial.print("[SX1262] Frequency error: ");
-				Serial.print(_radio.getSNR());
-				Serial.println(" Hz");
 
 				auto* packet = (GovnoPacket*) &buffer;
 
@@ -114,18 +130,8 @@ class Transceiver {
 					Serial.print("altitude: ");
 					Serial.println(packet->altitude);
 				}
-			} else if (state == RADIOLIB_ERR_RX_TIMEOUT) {
-				// timeout occurred while waiting for a packet
-				Serial.println(F("timeout!"));
-
-			} else if (state == RADIOLIB_ERR_CRC_MISMATCH) {
-				// packet was received, but is malformed
-				Serial.println(F("CRC error!"));
-
 			} else {
-				// some other error occurred
-				Serial.print(F("failed, code "));
-				Serial.println(state);
+				Serial.print("Not received");
 			}
 		}
 };
