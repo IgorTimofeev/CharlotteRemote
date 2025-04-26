fa76ed09e188c38b1fc029f29e26abb464d75b64
Igor Timofeev <methodman1@mail.ru>
2025-04-27 01:08:41 +0300

    ебанул полноэкранный 565 буфер, фпс до небес

--- main/main.cpp
+++ main/main.cpp
@@ -1,75 +1,67 @@
-#include "rc.h"
-
-extern "C" void app_main(void) {
-	pizda::RC::getInstance().run();
-}
-
-//#include <YOBA/main.h>
-//#include <YOBA/UI.h>
-//#include <YOBA/hardware/displays/ILI9341Display.h>
-//#include <YOBA/hardware/touchPanels/FT6336UTouchPanel.h>
-//#include <YOBA/resources/fonts/unscii16.h>
-//#include <esp_timer.h>
-//#include <esp_log.h>
-//
-//#include "constants.h"
-//
-//using namespace YOBA;
-//using namespace pizda;
-//
-//ILI9341Display _display = ILI9341Display(
-//	constants::spi::mosi,
-//	constants::spi::miso,
-//	constants::spi::sck,
-//
-//	constants::screen::slaveSelect,
-//	constants::screen::dataCommand,
-//	constants::screen::reset,
-//	constants::screen::frequency
-//);
-//
-//Bit8PaletteRenderer _renderer = Bit8PaletteRenderer(4);
-//
-//PaletteColor color0 = PaletteColor(0);
-//PaletteColor color1 = PaletteColor(1);
-//PaletteColor color2 = PaletteColor(2);
-//PaletteColor color3 = PaletteColor(3);
-//
-//unscii16Font font = unscii16Font();
-//
-//uint32_t govnoTime = 0;
+//#include "rc.h"
 //
 //extern "C" void app_main(void) {
-//	_display.setup();
-//	_renderer.setTarget(&_display);
-//
-//	_renderer.setPaletteColors({
-//		0xFF0000,
-//		0x00FF00,
-//		0x0000FF,
-//		0x000000
-//	});
-//
-//	ESP_LOGI("test", "getTransactionBufferHeight: %d", _renderer.getTransactionBufferHeight());
-//	ESP_LOGI("test", "getTransactionBufferLength: %d", _renderer.getTransactionBufferLength());
-//	ESP_LOGI("test", "getPaletteBufferLength: %d", _renderer.getPaletteBufferLength());
-//
-//	while (true) {
-//		// 1 - deltaTime
-//		// x - 1000000000
-//
-//		auto fps = govnoTime > 0 ? 1'000'000 / govnoTime : 0;
-//
-//		govnoTime = esp_timer_get_time();
-//
-//		_renderer.clear(&color0);
-////		_renderer.renderFilledRectangle(Bounds(_display.getSize()), &color1);
-//		_renderer.renderFilledRectangle(Bounds(10, 10, 30, 10), &color2);
-//		_renderer.renderString(Point(0, 0), &font, &color3, std::to_wstring(fps));
-//		_renderer.flush();
-//
-//		govnoTime = esp_timer_get_time() - govnoTime;
-//
-//		vTaskDelay(pdMS_TO_TICKS(1000));
-//	}
+//	pizda::RC::getInstance().run();
 //}
+
+#include <YOBA/main.h>
+#include <YOBA/UI.h>
+#include <YOBA/hardware/displays/ILI9341Display.h>
+#include <YOBA/hardware/touchPanels/FT6336UTouchPanel.h>
+#include <YOBA/resources/fonts/unscii16.h>
+#include <esp_timer.h>
+#include <esp_log.h>
+
+#include "constants.h"
+
+using namespace YOBA;
+using namespace pizda;
+
+ILI9341Display _display = ILI9341Display(
+	constants::spi::mosi,
+	constants::spi::miso,
+	constants::spi::sck,
+
+	constants::screen::slaveSelect,
+	constants::screen::dataCommand,
+	constants::screen::reset,
+	constants::screen::frequency,
+
+	Size(240, 320)
+);
+
+RGB565PixelBufferRenderer _renderer = RGB565PixelBufferRenderer();
+
+Rgb565Color color0 = Rgb888Color(0xFF0000).toRgb565();
+Rgb565Color color1 = Rgb888Color(0x00FF00).toRgb565();
+Rgb565Color color2 = Rgb888Color(0x0000FF).toRgb565();
+Rgb565Color color3 = Rgb888Color(0x000000).toRgb565();
+
+unscii16Font font = unscii16Font();
+
+uint32_t govnoTime = 0;
+
+extern "C" void app_main(void) {
+	_display.setup();
+	_renderer.setTarget(&_display);
+
+	while (true) {
+		// 1 - deltaTime
+		// x - 1000000000
+
+		auto fps = govnoTime > 0 ? 1'000'000 / govnoTime : 0;
+
+		govnoTime = esp_timer_get_time();
+
+		_renderer.clear(&color0);
+//		_renderer.renderFilledRectangle(Bounds(_display.getSize()), &color1);
+		_renderer.renderFilledRectangle(Bounds(10, 10, 30, 10), &color2);
+		_renderer.renderString(Point(0, 0), &font, &color3, std::to_wstring(fps));
+		_renderer.renderString(Point(0, 300), &font, &color3, std::to_wstring(esp_get_free_heap_size() / 1024));
+		_renderer.flush();
+
+		govnoTime = esp_timer_get_time() - govnoTime;
+
+		vTaskDelay(pdMS_TO_TICKS(1000));
+	}
+}
